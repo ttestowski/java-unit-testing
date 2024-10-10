1854bcb79956cef7c30f886e0009c57af6aeea6b
mmochocki <mmochocki@griddynamics.com>
2024-10-10 13:09:00 +0200

    add allure reporting

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # java-unit-testing
+
+This is a simple project to test jenkins job with build email notification and allure report.
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,3 +1,6 @@
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -6,26 +9,32 @@ class CalculatorTest {
     Calculator calculator = new Calculator();
 
     @Test
+    @DisplayName("Addition test")
     void testAddition() {
         assertEquals(5, calculator.add(2, 3), "2 + 3 should equal 5");
     }
 
     @Test
+    @DisplayName("Subtraction test")
     void testSubtraction() {
         assertEquals(1, calculator.subtract(3, 2), "3 - 2 should equal 1");
     }
 
     @Test
+    @DisplayName("Multiplication test")
     void testMultiplication() {
         assertEquals(5, calculator.multiply(2, 3), "2 * 3 should equal 6");
     }
 
     @Test
+    @DisplayName("Division test")
     void testDivision() {
         assertEquals(2.0, calculator.divide(6, 3), 0.001, "6 / 3 should equal 2.0");
     }
 
     @Test
+    @DisplayName("Division by zero test")
+    @Severity(SeverityLevel.CRITICAL)
     void testDivisionByZero() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
             calculator.divide(1, 0);
